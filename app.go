4dc951e0e2fd441b4de3e821d1821f25af23ae4e
leorenis <leorenis@gmail.com>
2020-05-03 18:17:28 -0300

    Learning collections with array and slices in Golang.

--- app.go
+++ app.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	showIntroduction()
-	showMenu()
 
-	command := readCommand()
-	switch command {
-	case 1:
-		fmt.Println("Mentoring")
-	case 2:
-		fmt.Println("Logs")
-	case 0:
-		os.Exit(0)
-	default:
-		os.Exit(-1)
+	for {
+		showMenu()
+
+		command := readCommand()
+		switch command {
+		case 1:
+			startMentoring()
+		case 2:
+			fmt.Println("Logs")
+		case 0:
+			os.Exit(0)
+		default:
+			os.Exit(-1)
+		}
 	}
 
 }
@@ -44,4 +48,20 @@ func readCommand() int {
 	fmt.Scan(&command)
 	fmt.Println("The command choosen:", command)
 	return command
+}
+
+func startMentoring() {
+	fmt.Println("Mentoring")
+
+	portals := []string{"https://google.com", "https://facebook.com", "https://twitter.com"}
+
+	for index, portal := range portals {
+		response, _ := http.Get(portal)
+		fmt.Println("Passing for slice position", index, "was found portal", portal)
+		if response.StatusCode == 200 {
+			fmt.Println("The site", portal, "its ok!")
+		} else {
+			fmt.Println("The site", portal, "its downtime. Status code: ", response.StatusCode)
+		}
+	}
 }
