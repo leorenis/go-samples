bd57fc7411a54cd89d57d6b5ed6851e00e0fdb68
leorenis <leorenis@gmail.com>
2020-05-03 15:22:24 -0300

    Setting short assignment statement

--- app.go
+++ app.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	var personName = "LEO R R Santos"
-	var appVersion float32 = 1.1
-	var age = 28
+	personName := "John Doe"
+	appVersion := 1.1
+	age := 28
 
 	fmt.Println("The first Go lang's app written by mr.", personName, "age", age)
 	fmt.Println("App Version: ", appVersion)
