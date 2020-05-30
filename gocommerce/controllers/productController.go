72aa6af33d82a47a373ce846a4f8ff623b9141f1
leorenis <leorenis@gmail.com>
2020-05-29 22:35:07 -0300

    Studing golang.

--- gocommerce/controllers/productController.go
+++ gocommerce/controllers/productController.go
@@ -43,3 +43,9 @@ func Insert(w http.ResponseWriter, req *http.Request) {
 	}
 	http.Redirect(w, req, "/", 301)
 }
+
+// Delete is
+func Delete(w http.ResponseWriter, req *http.Request) {
+	id := req.FormValue("id")
+	log.Println(id)
+}
