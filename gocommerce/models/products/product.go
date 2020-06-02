f3c660694833a297c6b15215f8ce994f8627e517
leorenis <leorenis@gmail.com>
2020-06-01 22:00:45 -0300

    Studing golang.

--- gocommerce/models/products/product.go
+++ gocommerce/models/products/product.go
@@ -1,7 +1,9 @@
 package products
 
 import (
+	"database/sql"
 	"gosamples/gocommerce/db"
+	"log"
 )
 
 // Product is
@@ -68,29 +70,33 @@ func Delete(id string) {
 }
 
 // FindByID is
-func FindByID(id string) Product {
+func FindByID(seqID string) Product {
 	db := db.OpenDBConnection()
-	row, err := db.Query("SELECT * FROM products WHERE id = ?", id)
-	if nil != err {
-		panic(err)
-	}
+
+	var id, amount int
+	var name, description string
+	var price float64
+
 	product := Product{}
-	for row.Next() {
-		var id, amount int
-		var name, description string
-		var price float64
 
-		err = row.Scan(&id, &name, &price, &amount, &description)
-		if err != nil {
-			panic(err.Error())
-		}
+	row := db.QueryRow("SELECT * FROM products WHERE id = ?", seqID)
 
+	switch err := row.Scan(&id, &name, &price, &amount, &description); err {
+
+	case sql.ErrNoRows:
+		log.Println("No rows were returned")
+
+	case nil:
 		product.ID = id
 		product.Name = name
 		product.Description = description
 		product.Price = price
 		product.Amount = amount
+
+	default:
+		panic(err)
 	}
+
 	defer db.Close()
 	return product
 }
