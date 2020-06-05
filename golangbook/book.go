2d83435c6c59da3847a1972a0868db9f62d51262
leorenis <leorenis@gmail.com>
2020-06-05 10:18:38 -0300

    Studing golang.

--- golangbook/book.go
+++ golangbook/book.go
@@ -5,4 +5,5 @@ import "gosamples/golangbook/ch1"
 func main() {
 	ch1.SliceRagesTesteds()
 	ch1.Echo1()
+	ch1.Echo2()
 }
--- golangbook/ch1/echo1.go
+++ golangbook/ch1/echo1.go
@@ -1,3 +1,4 @@
+// Book's exemples for the Chapter One
 package ch1
 
 import (
@@ -16,6 +17,16 @@ func Echo1() {
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+}
+
+// Echo2 is
+func Echo2() {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
