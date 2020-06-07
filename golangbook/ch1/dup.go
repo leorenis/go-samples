f49f7c195ddb5585c853d292a72907239a8bc58b
leorenis <leorenis@gmail.com>
2020-06-07 12:47:49 -0300

    Studing golang.

--- golangbook/ch1/dup.go
+++ golangbook/ch1/dup.go
@@ -9,7 +9,7 @@ import (
 
 // ShowDup is
 func ShowDup() {
-	dup1()
+	dup2()
 }
 
 // Dup1 is a function exibe o texto de toda linha que aparece mais de uma vez na entrada padrao, precedida por sua contagem.
@@ -27,5 +27,38 @@ func dup1() {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
+	}
+}
+
+// Dup2 is
+// Exibe a contagem e o texto das linhas que aparecem mais de uma vez na entrada. Ele lê StdIn ou uma lista de arquivos nomeados.
+func dup2() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
 	}
 }
