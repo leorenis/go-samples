f3ec5071ec5f79dda0820788d41229dc35dd9e2d
leorenis <leorenis@gmail.com>
2020-06-16 09:34:56 -0300

    Studying.

--- golangbook/ch2/ftoc.go
+++ golangbook/ch2/ftoc.go
@@ -7,8 +7,18 @@ func ShowFtoc() {
 	const freezingF, boilingF = 32.0, 212.0
 	fmt.Printf("%g •F = %g •C \n", freezingF, fToC(freezingF)) // "32 •F = 0•C "
 	fmt.Printf("%g •F = %g •C \n", boilingF, fToC(boilingF))   // "212 •F = 100•C "
+
+	// Curiosity
+	pointers()
 }
 
 func fToC(f float64) float64 {
 	return (f - 32) * 5 / 9
+}
+
+func pointers() {
+	x := 1
+	p := &x
+	fmt.Println("p has a address for x: ", p, " *p pointer for x value as you can see: *p=", *p, " and x=", x)
+	fmt.Printf("%d \n", p) // Var p is a pointer for a int variable
 }
