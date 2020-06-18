a283383aaef73300d627c5cecb2a9b249f18fa4c
leorenis <leorenis@gmail.com>
2020-06-18 09:35:44 -0300

    Studying.

--- golangbook/book.go
+++ golangbook/book.go
@@ -12,6 +12,6 @@ func main() {
 	// ch1.StartServer()
 
 	// ch2.ShowBoiling()
-	// ch2.ShowFtoc()
-	ch2.ShowEchoFourth()
+	ch2.ShowFtoc()
+	// ch2.ShowEchoFourth()
 }
--- golangbook/ch2/ftoc.go
+++ golangbook/ch2/ftoc.go
@@ -21,6 +21,7 @@ func pointers() {
 	p := &x
 	fmt.Println("p has a address for x: ", p, " *p pointer for x value as you can see: *p=", *p, " and x=", x)
 	fmt.Printf("%d \n", p) // Var p is a pointer for a int variable
+	fmt.Println("Samething still crazy: ", &*p, &*p == p)
 
 	var xs, xy int
 	fmt.Println(&xs == &xs, &xs == &xy, &xs == nil)
