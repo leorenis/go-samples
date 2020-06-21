dcf9f96c6ce1643ecb60a9e713fc77c256026d29
leorenis <leorenis@gmail.com>
2020-06-21 20:04:01 -0300

    Studying.

--- golangbook/ch2/tempconv0.go
+++ golangbook/ch2/tempconv0.go
@@ -23,7 +23,7 @@ func ShowTempConv() {
 	boilingF := ctoF(Boiling)
 	fmt.Printf("%g \n", boilingF-ctoF(FreezingC)) // 180•C
 	// fmt.Printf("%g \n", boilingF-FreezingC) // Erro de compilacao. Incompatibilidade de tipos
-	Boiling.string()
+	fmt.Println(Boiling.string())
 }
 
 func ctoF(c Celsius) Fahrenheit          { return Fahrenheit(c*9/5 + 32) }
