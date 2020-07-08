33498d9905ea6c5506cb43f28af0d5f3ee128c95
leorenis <leorenis@gmail.com>
2020-07-08 10:20:46 -0300

    Studying.

--- golangbook/ch2/scope.go
+++ golangbook/ch2/scope.go
@@ -56,7 +56,7 @@ var cwd2 string
 
 func init() {
 	var err error
-	cwd, err = os.Getwd()
+	cwd2, err = os.Getwd()
 	if err != nil {
 		log.Fatalf("os.Getwd failed: %v", err)
 	}
---
+++ golangbook/ch3/overflow.go
@@ -0,0 +1,9 @@
+package ch3
+
+import "fmt"
+
+// ShowOverflow is
+func ShowOverflow() {
+	var n uint8 = 255
+	fmt.Println(n)
+}
