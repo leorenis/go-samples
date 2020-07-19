88e24b49258182a2c73fbcaa5c135489d4d32977
leorenis <leorenis@gmail.com>
2020-07-18 23:02:45 -0300

    Studying.

--- golangbook/ch3/bitwise.go
+++ golangbook/ch3/bitwise.go
@@ -11,4 +11,8 @@ func ShowBitwise() {
 	fmt.Printf("%08b\n", x) // 00100010, o conjunto {1. 5}
 	fmt.Printf("%08b\n", y) // 00000110, o conjunto {1. 2}
 
+	fmt.Printf("%08b\n", x&y)  // 00000010, a interseccao(1)
+	fmt.Printf("%08b\n", x|y)  // 00100110, a uniao(1,2,5}
+	fmt.Printf("%08b\n", x^y)  // 00100100, a diferenca simetrica(2,5}
+	fmt.Printf("%08b\n", x&^y) // 00100000, a diferenca (5}. O operador &^ serve para limpeza de bits (AND NOT): Na expressao `z=x&ˆy`, cada bit de Z será 0 se o bit correspondente de Y for 1. Caso contrario, será o bit correspondente de X.
 }
