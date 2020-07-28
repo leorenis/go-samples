9a1a9e947837dd80e5c99932493f3fab73c46767
leorenis <leorenis@gmail.com>
2020-07-28 09:55:35 -0300

    Studying.

--- golangbook/ch3/types.go
+++ golangbook/ch3/types.go
@@ -24,4 +24,12 @@ func accuracyLoss() {
 	fmt.Println(f, i)
 	f = 1.99
 	fmt.Println(int(f))
+
+	f = 1e100
+	i = int(f)
+	fmt.Println(f, i)
 }
+
+// Note:
+//      Conversoes de ponto flutuante para inteiro descartam qualquer parte fracionaria.
+//      Truncando o resultado na direcao do zero.
