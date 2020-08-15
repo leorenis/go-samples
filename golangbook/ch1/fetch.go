731de5e43414af0ad10978b5c0a9f61bade9e773
leorenis <leorenis@gmail.com>
2020-08-14 21:24:39 -0300

    Golang studying.

--- golangbook/ch1/fetch.go
+++ golangbook/ch1/fetch.go
@@ -15,20 +15,16 @@ import (
 func Fetch() {
 	for _, url := range os.Args[1:] {
 		resp, err := http.Get(urlNormalized(url))
-
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
-
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v \n", url, err)
 			os.Exit(1)
 		}
-
 		fmt.Printf("\nHTTP status: %s", resp.Status)
 	}
 }
